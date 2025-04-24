b9f2cfd255c01a423fb7dafb98add3833d9b401e
Yamoon224 <Yamooon664@gmail.com>
2025-04-24 13:47:02 +0000

    modified:   app/layout.tsx 	new file:   package-lock.json 	new file:   public/favicon.png

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="!scroll-smooth">
+      <head>
+        <link rel="icon" href="/favicon.png" />
+      </head>
       <body className={cn(inter.className, "min-h-screen bg-background font-sans antialiased")}>{children}</body>
     </html>
   )
